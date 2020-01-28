ee9c3ccf043477f646433c3769192c786ad5679c
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2020-01-28 20:10:54 +0100

    Flow added

--- src/Workshop/Domain.fs
+++ src/Workshop/Domain.fs
@@ -37,6 +37,18 @@ type Account = {
     BirthDateUtc: DateTime option
     ContactInfo: ContactInfo
 }
+/// Anemic domain model reference ?
+/// 3
+/// FLOW OF STAGE 1
+/// 0) Open your giraffe project, create Domain.fs
+/// 1) Install FsharpPlus
+/// 2) Specification, tell them that this will be saved in database
+/// 3) Discuss some solutions, show this one
+/// 4) How can invalid state be achieved here ? How can we prevent it ? Is it worth it ?
+/// 5) Smart constructors as a compromise
+/// 6) Why type aliases on guid ?
+/// 7) Cyclic dependencies, why are they wrong ? Show typical usecase with 1-N relationship in EF, cannot be done with immutability properly.
+/// 8) Modeling data as data, not references etc. follow-up on point 7
 
 // Lens usecase - update single Address
 // Account.ContactInfo.Addresses[addressId].Street = "new value"
