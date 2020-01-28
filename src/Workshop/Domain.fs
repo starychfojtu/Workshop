a3c960976ae69a7a6ced46c7de7d2e73b37ce2f9
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2020-01-28 20:43:50 +0100

    Create functions

--- src/Workshop/Domain.fs
+++ src/Workshop/Domain.fs
@@ -4,12 +4,59 @@ open System
 open FSharpPlus.Data
 open System.Net.Mail
 
-// Person registry - contact info
+/// Anemic domain model reference ?
+/// FLOW OF STAGE 1
+/// - Open your giraffe project, create Domain.fs
+/// - Install FsharpPlus
+/// - Specification, Domain-first approach, we will choose specifics later
+/// - Discuss some solutions, show this one
+/// - How can invalid state be achieved here ? How can we prevent it ? Is it worth it ? (e.g. ZipCode)
+/// - Smart constructors as a compromise
+/// - Why type aliases on guid ?
+/// - Mention of NodaTime
+/// - Cyclic dependencies, why are they wrong ? Show typical usecase with 1-N relationship in EF, cannot be done with immutability properly.
+/// - Modeling data as data, not references etc. follow-up on point 7
+///
+/// FLOW OF STAGE 2
+/// - Sub modules of all types
+/// - .Value functions
+/// - Securing constructors with private keyword
+/// - Why create methods ? record type constructors are not a functions (lang proposal on github), we will operate with functions
+/// - Discussion: Why don't we validate birth date ? (Side effect, valid use-cases with birth date in future etc.) 
 
-// Aliasing, smart constructor, can be improved - try it
-type NonEmptyString = NonEmptyString of string
-type PhoneNumber = Telephone of NonEmptyString
-type ZipCode = ZipCode of NonEmptyString
+type NonEmptyString = private NonEmptyString of string
+    with member s.Value = match s with NonEmptyString v -> v
+
+module NonEmptyString =
+    let create s =
+        if String.IsNullOrEmpty s  
+            then None
+            else Some <| NonEmptyString s
+
+type PhoneNumber = private PhoneNumber of NonEmptyString
+    with member s.Value = match s with PhoneNumber v -> v
+
+module PhoneNumber =
+    let private isValid (s: NonEmptyString) =
+        String.length s.Value = 13 && s.Value.StartsWith "+420"
+            
+    let create s =
+        if isValid s
+            then Some <| PhoneNumber s
+            else None
+
+type ZipCode = private ZipCode of NonEmptyString
+    with member s.Value = match s with ZipCode v -> v
+    
+module ZipCode =
+    let private isValid (s: NonEmptyString) =
+        let chars = s.Value.ToCharArray()
+        String.length s.Value = 5 && Seq.forall Char.IsDigit chars
+            
+    let create s =
+        if isValid s
+            then Some <| ZipCode s
+            else None
 
 type Address = {
     Street: NonEmptyString
@@ -17,6 +64,14 @@ type Address = {
     City: NonEmptyString
     ZipCode: ZipCode
 }
+
+module Address =
+    let create street homeNumber city zipCode = {
+        Street = street
+        HomeNumber = homeNumber
+        City = city
+        ZipCode = zipCode
+    }
 
 type ContactMethod =
     | Email of MailAddress * PhoneNumber option
@@ -27,8 +82,13 @@ type ContactInfo = {
     Addresses: NonEmptyList<Address> 
 }
 
+module ContactInfo =
+    let create method addresses = {
+        ContactMethod = method
+        Addresses = addresses
+    }
+    
 type AccountId = AccountId of Guid
-type AccountName = AccountName of string
 
 type Account = {
     Id: AccountId
@@ -37,18 +97,15 @@ type Account = {
     BirthDateUtc: DateTime option
     ContactInfo: ContactInfo
 }
-/// Anemic domain model reference ?
-/// 3
-/// FLOW OF STAGE 1
-/// 0) Open your giraffe project, create Domain.fs
-/// 1) Install FsharpPlus
-/// 2) Specification, tell them that this will be saved in database
-/// 3) Discuss some solutions, show this one
-/// 4) How can invalid state be achieved here ? How can we prevent it ? Is it worth it ?
-/// 5) Smart constructors as a compromise
-/// 6) Why type aliases on guid ?
-/// 7) Cyclic dependencies, why are they wrong ? Show typical usecase with 1-N relationship in EF, cannot be done with immutability properly.
-/// 8) Modeling data as data, not references etc. follow-up on point 7
+
+module Account =
+    let create id firstName lastName birthDateUtc contactInfo = {
+        Id = id
+        FirstName = firstName
+        LastName = lastName
+        BirthDateUtc = birthDateUtc
+        ContactInfo = contactInfo
+    }
 
 // Lens usecase - update single Address
 // Account.ContactInfo.Addresses[addressId].Street = "new value"
